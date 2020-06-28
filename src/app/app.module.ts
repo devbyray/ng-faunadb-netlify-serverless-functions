599168f52e37d86dd50677453c0cbd1863a50761
Raymon S <raymon@raymonschouwenaar.nl>
2020-06-28 21:46:34 +0200

    Show products in list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'
 import { ProductListComponent } from './products/components/product-list/product-list.component'
+import { MatGridListModule } from '@angular/material/grid-list'
 @NgModule({
 	declarations: [AppComponent, ProductListComponent],
-	imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule],
+	imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, MatGridListModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
